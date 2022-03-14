7a2dd9c758a3d8717d3355942cc6e05fa2faf0c5
kemalgencay <kemalgencay@gmail.com>
2022-03-14 09:27:34 +0300

    Bu ilk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './style';
+import "./style"
 
 export default function App() {
-	return (
-		<div>
-			<h1>Hello, World!</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Şiktir, World!</h1>
+    </div>
+  )
 }
